1fa7e8127653da1d20309f3e31e02b5956dd742f
SiveyDev <sivey@protonmail.com>
2021-11-01 19:09:27 -0400

    Creat Book constructor and function to add book object to library array

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-alert("Books are fun!");
+let myLibrary = [];
+
+addBookToLibrary();
+
+function Book (title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary() {
+    let title = prompt("Enter book title: ");
+    let author = prompt("Enter book author");
+    let pages = Number(prompt("Enter number of paages in the book"));
+    let readCheck = prompt("Have you read this book?");
+    console.log(readCheck);
+    let hasRead;
+    if (readCheck.toLowerCase() === 'yes') {
+        hasRead = true;
+    } else if (readCheck.toLowerCase() === 'no') {
+        hasRead = false;
+    } else {
+        prompt("Invalid answer. Have you read this book?");
+    }
+
+    let myBook = new Book(title, author, pages, hasRead);
+    myLibrary.push(myBook);
+    console.log(myLibrary);
+}
